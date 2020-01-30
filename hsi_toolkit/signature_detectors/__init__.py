19ee1701e3896215fe824a52107f1dc1319de6ed
HaleySchuhl <haleyschuhl@gmail.com>
2020-01-30 11:09:26 -0600

    comment out other imports for control

--- hsi_toolkit/signature_detectors/__init__.py
+++ hsi_toolkit/signature_detectors/__init__.py
@@ -1,25 +1,25 @@
-from abd_detector import *
-from ace_detector import *
-from ace_local_detector import *
-from ace_rt_detector import *
-from ace_rt_max_detector import *
-from ace_ss_detector import *
-from amsd_detector import *
-from cem_detector import *
-from ccmf_detector import *
-from ctmf_detector import *
-from fam_statistic import *
-from ftmf_detector import *
-from ha_detector import *
-from hua_detector import *
-from hsd_detector import *
-from hsd_local_detector import *
-from mtmf_statistic import *
-from osp_detector import *
-from qmf_detector import *
-from sam_detector import *
-from smf_detector import *
-from smf_local_detector import *
-from smf_max_detector import *
-from spsmf_detector import *
-from palm_detector import *
+# from abd_detector import *
+# from ace_detector import *
+# from ace_local_detector import *
+# from ace_rt_detector import *
+# from ace_rt_max_detector import *
+# from ace_ss_detector import *
+# from amsd_detector import *
+# from cem_detector import *
+# from ccmf_detector import *
+# from ctmf_detector import *
+# from fam_statistic import *
+# from ftmf_detector import *
+# from ha_detector import *
+# from hua_detector import *
+# from hsd_detector import *
+# from hsd_local_detector import *
+# from mtmf_statistic import *
+# from osp_detector import *
+# from qmf_detector import *
+# from sam_detector import *
+# from smf_detector import *
+# from smf_local_detector import *
+# from smf_max_detector import *
+# from spsmf_detector import *
+# from palm_detector import *
