44a0e6d3a4438b14ac20135cde018b7ca75404a2
HaleySchuhl <haleyschuhl@gmail.com>
2020-06-04 12:30:32 -0500

    add requirements and setup files

---
+++ setup.py
@@ -0,0 +1,137 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+import sys
+import setuptools
+#from setuptools.command.test import test as TestCommand
+from codecs import open
+from os import path
+
+
+# class PyTest(TestCommand):
+#     def initialize_options(self):
+#         TestCommand.initialize_options(self)
+#         self.pytest_args = ["--verbose", "tests/tests.py"]
+#
+#     def finalize_options(self):
+#         TestCommand.finalize_options(self)
+#         self.test_args = []
+#         self.test_suite = True
+#
+#     def run_tests(self):
+#         import pytest
+#         errno = pytest.main(self.pytest_args)
+#         sys.exit(errno)
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requirements_f = open('requirements.txt', 'r')
+dependencies = [ req for req in requirements_f.readlines() ]
+
+setuptools.setup(
+    name='hsi_toolkit',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+
+    description='GatorSense Hyperspectral Image Analysis Toolkit - Python Implementation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    # The project's main homepage.
+    url='https://faculty.eng.ufl.edu/machine-learning/',
+
+    # Author details
+    author='The GatorSense team',
+    author_email='azare@ufl.edu',
+
+    # Choose your license
+    license='MIT',
+
+    # Supported platforms
+    platforms=['Any'],
+
+    # Version number
+    version=1.0,
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+
+    # What does your project relate to?
+    keywords='plant phenotyping bioinformatics hyperspectral machine-learning sensing',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=setuptools.find_packages(),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=dependencies,
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'test': ['pytest-runner', 'pytest'],
+    # },
+    setup_requires=["pytest-runner"],
+    tests_require=['pytest'],
+    ############ scripts=["plantcv-train.py", "plantcv-utils.py", "plantcv-workflow.py"]
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+)
